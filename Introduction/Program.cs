2544a71ff420a053954551a70676c27b50c98ef7
tekater <tekateplay1@yandex.ru>
2023-09-23 00:53:47 +0900

    hw

--- Introduction/Program.cs
+++ Introduction/Program.cs
@@ -1,4 +1,5 @@
 ﻿//#define CONSOLE_SETTINGS
+//#define WALTER_WHITE_50
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -56,6 +57,7 @@ namespace Introduction
             Console.ResetColor();
 #endif
 
+#if WALTER_WHITE_50
 
             Console.Write("Введите ваше ИМЯ: ");
             string first_name = Console.ReadLine();
@@ -74,6 +76,241 @@ namespace Introduction
 
             //3) Интерполяция строк:
             Console.WriteLine($"Имя: {first_name}, фамилия: {last_name} , возраст: {age} лет.");
+
+
+
+
+
+#endif
+
+            char measurement;
+
+            do
+            {
+                measurement = Console.ReadKey().KeyChar;
+                switch (measurement)
+                {
+                    case 'w':
+                        Console.WriteLine(" - Вверх");
+                        break;
+                    case 'a':
+                        Console.WriteLine(" - Влево");
+                        break;
+                    case 'd':
+                        Console.WriteLine(" - Вправо");
+                        break;
+
+                    case 's':
+                        Console.WriteLine(" - Вниз");
+                        break;
+                    case ' ':
+                        Console.WriteLine(" - Прыжок");
+                        break;
+                    case '\r':
+                        Console.WriteLine(" - Выстрел");
+                        break;
+                    case 'q':
+                        Console.WriteLine(" - Exit");
+                        break;
+                    default:
+                        Console.WriteLine(" - Error");
+                        break;
+                }
+            } while (measurement != 'q');
+
+            Console.WriteLine("\n\nGeometry:\n(0):\n");
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("\n(1):\n");
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if (i >= j)
+                    {
+                        Console.Write("*");
+                    }
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("\n(2):\n");
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if (i <= j)
+                    {
+                        Console.Write("*");
+                    }
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("\n(3):\n");
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if (i <= j)
+                    {
+                        Console.Write("*");
+
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("\n(4):\n");
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = i; j < 5; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 0; j < 5; j++)
+                {
+                    if (j <= i)
+                    {
+                        Console.Write("*");
+
+                    }
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("\n(5):\n");
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = i; j < 5 - 1; j++)
+                {
+                    Console.Write(" ");
+                }
+                Console.Write("/");
+                for (int j = 0; j < i * 2; j++)
+                {
+                    Console.Write(" ");
+                }
+                Console.Write("\\");
+                Console.WriteLine();
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j <= i - 1; j++)
+                {
+                    Console.Write(" ");
+                }
+                Console.Write("\\");
+                for (int j = (i + 1) * 2; j < (5) * 2; j++)
+                {
+                    Console.Write(" ");
+                }
+                Console.Write("/");
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("\n(6):\n");
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j <= 5; j++)
+                {
+                    if ((i + j) % 2 == 0)
+                    {
+                        Console.Write("+ ");
+                    }
+                    else
+                    {
+                        Console.Write("- ");
+                    }
+                }
+                Console.WriteLine();
+            }
+
+
+
+            {
+                Console.WriteLine("Введите размер Шахматной доски:\n");
+                int n = Convert.ToInt32(Console.ReadLine());
+
+                for (int i = 0; i <= n; i++)
+                {
+                    for (int j = 0; j <= n; j++)
+                    {
+                        if (i == 0 && j == 0)
+                        {
+                            Console.Write('+');
+                        }
+                        else if (i == 0 && j == n)
+                        {
+                            Console.Write('+');
+                        }
+                        else if (i == n && j == 0)
+                        {
+                            Console.Write('+');
+                        }
+                        else if (i == n && j == n)
+                        {
+                            Console.Write('+');
+                        }
+                        else if (i == 0 || i == n)
+                        {
+                            Console.Write("-" + "-");
+                        }
+                        else if (j == 0 || j == n)
+                        {
+                            Console.Write("|");
+                        }
+                        else if ((i + j) % 2 == 0)
+                        {
+                            Console.Write("#" + "#");
+                        }
+                        else
+                        {
+                            Console.Write(" " + " ");
+                        }
+                    }
+                    Console.WriteLine();
+                }
+            }
+
+
+
+            {
+                Console.WriteLine("Введите размер Шахматной доски:\n");
+                int n = Convert.ToInt32(Console.ReadLine());
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        for (int k = 0; k < n; k++)
+                        {
+                            for (int l = 0; l < n; l++)
+                            {
+                                if ((i + k) % 2 == 0)
+                                {
+                                    Console.Write("* ");
+                                }
+                                else 
+                                {
+                                    Console.Write(" ");
+                                }
+                            }
+                        }
+                        Console.WriteLine();
+                    }
+                }
+            }
         }
     }
 }
