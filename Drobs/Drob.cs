6e0a6bd751a5635db1d784737696e13ee4b28b1c
tekater <tekateplay1@yandex.ru>
2023-10-02 23:36:54 +0900

    Drobs

---
+++ Drobs/Drob.cs
@@ -0,0 +1,347 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace Drob
+{
+	internal class Drob
+	{
+		int znamen;
+
+		public int Ans
+		{
+			/*
+			get { return ans; }
+			set { ans = value; }
+			*/
+			get;
+			set;
+		}
+		public int Chislit
+		{
+			get;
+			set;
+		}
+		public int Znamen
+		{
+			get { return znamen; }
+			set { 
+				if(value == 0)
+				{
+					value = 1;
+				}
+				znamen = value; 
+			}
+		}
+
+		//				Constructor:
+		public Drob()
+		{
+			Ans = 0;
+			Chislit = 0;
+			Znamen = 1;
+			Console.WriteLine("Def Constructor");
+		}
+		public Drob(int ans)
+		{
+			Ans = ans;
+			Znamen = 0;
+			Chislit = 0;
+			Console.WriteLine("1Arg Constructor");
+		}
+		public Drob(int chislit, int znamenat)
+		{
+			Ans = 0;
+			Znamen = znamenat;
+			Chislit = chislit;
+			Console.WriteLine("2Arg Constructor");
+		}
+		public Drob(int ans, int chislit, int znamenat)
+		{
+			Chislit = (chislit);
+			Znamen = (znamenat);
+			Ans = (ans);
+			Console.WriteLine("3Arg Constructor");
+		}
+		public Drob(double num)
+		{
+			Console.WriteLine("Double Constructor");
+			Ans = (int)num;
+			for (int i = 1; i < 10; i++)
+			{
+				for (int j = 1; j < 10; j++)
+				{
+					if ((double)i / j == (num - (int)num))
+					{			
+						Chislit = i;
+						Znamen = j;
+						return;
+					}
+				}
+			}
+		}
+
+		public Drob(Drob other)
+		{
+			this.Ans = other.Ans;
+			this.Chislit = other.Chislit;
+			this.Znamen = other.Znamen;
+            Console.WriteLine("Copy Constructor");
+        }
+
+		//				Operators
+
+		public static Drob operator *(Drob left,Drob right)
+		{
+			Drob left_copy = new Drob();
+			Drob right_copy = new Drob();
+			left_copy.toImproper();
+			right_copy.toImproper();
+			return new Drob(left_copy.Chislit * right_copy.Chislit, left_copy.Znamen * right_copy.Znamen).toProper();
+		}
+
+		public static Drob operator /(Drob left,Drob right)
+		{
+			return left * right.Inverted();
+		}
+
+
+		//				Methods:
+
+		public Drob toProper()
+		{
+			Ans += Chislit / Znamen;
+			Chislit %= Znamen;
+			return this;
+		}
+
+		public Drob toImproper()
+		{
+			Chislit += Ans * Znamen;
+			Ans = 0;
+			return this;
+		}
+
+		public Drob Inverted()
+		{
+			Drob inverted = new Drob(this);
+			inverted.toImproper();
+			(inverted.Chislit, inverted.Znamen) = (inverted.Znamen, inverted.Chislit);
+			return inverted;
+		}
+
+		public void Print()
+		{
+			if (Ans != 0)
+			{
+				Console.Write(Ans);
+			}
+			if (Chislit != 0)
+			{
+				if (Ans != 0)
+				{
+					Console.Write($"(");
+				}
+				Console.Write($"{Chislit} / {Znamen}");
+				if (Ans != 0)
+				{
+					Console.Write($")");
+				}
+			}
+			else if (Ans == 0)
+			{
+				Console.Write(0);
+			}
+			Console.WriteLine("\n");
+		}
+		public void Print(int chislit,int znamen,int ans = 0)
+		{
+            Console.Write($"my drob:\n");
+            if (Ans != 0)
+			{
+				Console.Write(Ans);
+			}
+			if (Chislit != 0)
+			{
+				if (Ans != 0)
+				{
+					Console.Write($"(");
+				}
+				Console.Write($"{Chislit} / {Znamen}");
+				if (Ans != 0)
+				{
+					Console.Write($")");
+				}
+			}
+			else if (Ans == 0)
+			{
+				Console.Write(0);
+			}
+
+			Console.WriteLine("\nsecond Drob");
+
+			if (ans != 0)
+			{
+				Console.Write(ans);
+			}
+			if (chislit != 0)
+			{
+				if (ans != 0)
+				{
+					Console.Write($"(");
+				}
+				Console.Write($"{chislit} / {znamen}");
+				if (ans != 0)
+				{
+					Console.Write($")");
+				}
+			}
+			else if (ans == 0)
+			{
+				Console.Write(0);
+			}
+            Console.WriteLine("\n");
+        }
+
+		/////////////////////////////
+
+		public override string ToString()
+		{
+			string output = "";
+			if (Ans != 0)
+			{
+				output += Ans.ToString();
+			}
+			if (Chislit != 0)
+			{
+				if (Ans != 0)
+				{
+					Console.Write($"(");
+				}
+				Console.Write($"{Chislit} / {Znamen}");
+				if (Ans != 0)
+				{
+					Console.Write($")");
+				}
+			}
+			else if (Ans == 0)
+			{
+				output = "0";
+			}
+			Console.WriteLine("\n");
+
+			return output;
+		}
+
+		/////////////////////////////
+
+
+		public double mult_(int chislit, int znamen)
+		{
+			return ((double)Chislit / Znamen) * ((double)chislit / znamen);
+		}
+		public double min_(int chislit, int znamen)
+		{
+			return ((double)Chislit / Znamen) - ((double)chislit / znamen);
+		}
+
+		public double del_(int chislit, int znamen)
+		{
+			return ((double)Chislit / Znamen) / (Chislit / znamen);
+		}
+
+		public double plus_(int chislit, int znamen)
+		{
+			return ((double)Chislit / Znamen) + ((double)chislit / znamen);
+		}
+
+		/////////////////////////////
+		//			MegaPrints
+		/////////////////////////////
+		public void Fplus(int chislit, int znamen, int ans = 0)
+		{
+			Console.Write($"Plus_Full:\n");
+
+			Chislit += Ans * Znamen;
+			Ans = 0;
+
+			chislit += ans * znamen;
+			ans = 0;
+
+			Print(chislit, znamen,ans);
+
+			Console.Write($"{ Chislit} * {znamen} + {chislit} * {Znamen}");
+			Console.Write($"\n------------\t =");
+            Console.Write(plus_(chislit,znamen));
+			Console.WriteLine($"\n{ znamen} * { Znamen}");
+
+			Console.WriteLine("\n");
+
+		}
+
+		public void Fmin(int chislit, int znamen,int ans = 0)
+		{
+			Console.Write($"Min_Full:\n");
+
+			Chislit += Ans * Znamen;
+			Ans = 0;
+
+			chislit += ans * znamen;
+			ans = 0;
+
+			Print(chislit, znamen, ans);
+
+			Console.Write($"{Chislit} * {znamen} - {chislit} * {Znamen}");
+			Console.Write($"\n------------\t =");
+			Console.Write(min_(chislit, znamen));
+			Console.WriteLine($"\n{znamen} * {Znamen}");
+
+            Console.WriteLine("\n");
+        }
+
+		public void Fmult(int chislit, int znamen, int ans = 0)
+		{
+			Console.Write($"Mult_Full:\n");
+
+			Chislit += Ans * Znamen;
+			Ans = 0;
+
+			chislit += ans * znamen;
+			ans = 0;
+
+			Print(chislit, znamen, ans);
+
+			Console.Write($"{Chislit} * {chislit}");
+			Console.Write($"\n------------\t =");
+			Console.Write(mult_(chislit, znamen));
+			Console.WriteLine($"\n{znamen} * {Znamen}");
+
+			Console.WriteLine("\n");
+		}
+
+		public void Fdel(int chislit, int znamen, int ans = 0)
+		{
+			Console.Write($"Del_Full:\n");
+
+			Chislit += Ans * Znamen;
+			Ans = 0;
+
+			chislit += ans * znamen;
+			ans = 0;
+
+			Print(chislit, znamen, ans);
+
+			Console.Write($"{ Chislit} * {znamen}");
+			Console.Write($"\n------------\t =");
+			Console.Write(del_(chislit, znamen));
+            Console.WriteLine($"\n{Znamen} * {chislit} ");
+
+			Console.WriteLine("\n");
+		}
+
+	}
+}
--- Drobs/Program.cs
+++ Drobs/Program.cs
@@ -1,4 +1,4 @@
-﻿using Drobs.std;
+﻿
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,149 +6,48 @@ using System.Runtime.InteropServices.ComTypes;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Drobs
+namespace Drob
 {
 	internal class Program
 	{
 		static void Main(string[] args)
 		{
+			Drob A = new Drob();
+			//A.setAns(22);
+			//A.setChislit(33);
+			//A.setZnamenat(44);
 
-			Console.WriteLine($"Равны: {(d == d2)}");
-			Console.WriteLine($"Принт: {d}");
+			A.Ans = 123;
+			A.Chislit = 456;
+			A.Znamen = 789;
 
-			//d++;
-			//Console.WriteLine($"++{d}");
+			A.Print();
 
-			//Console.WriteLine($"{d}");
+			Console.WriteLine("\n-----------------------\n");
 
-			//d + d2;
-			//Console.WriteLine($"Плюс: {d}");
 
-			//d.setCh(4);
-			//d.setZn(8);
+			Drob B = new Drob(5);
+			B.Print();
 
-			//d* d2;
-			//Console.WriteLine($"Мульт: {d}");
+			Console.WriteLine("\n-----------------------\n");
+
+			Drob C = new Drob(2,3,5);
+			C.Print();
+			C.Fdel(1,2);
+			C.Fmult(1, 2);
+			C.Fplus(1, 2);
+			C.Fmin(1, 2);
+
+            Console.WriteLine("\n-----------------------\n");
+
+            Drob D = new Drob(2,3,5);
+			D.Print();
+
+			Drob G = new Drob(2.75);
+			G.Print();
 		}
-		/*
 
-		double mult(const Drob& d)
-    {
-        return (double(chislit) / znamenat) * (double(d.chislit) / d.znamenat);
-    }
 
-	double min_(const Drob& d)
-	{
-		return double(chislit) / znamenat - double(d.chislit) / d.znamenat;
 	}
-	double del(const Drob& d)
-	{
-		return (double(chislit) / znamenat) / (double(d.chislit) / d.znamenat);
-	}
-	double plus_(const Drob& d)
-	{
-		return double(chislit) / znamenat + double(d.chislit) / d.znamenat;
-	}
-	void print(const Drob& d)
-	{
-		cout << d.chislit << "/" << d.znamenat << "\n";
-	}
-	void Fplus(const Drob& d)
-	{
-		cout << "Plus_Full:\t\b\b\b\b" << chislit * d.znamenat + d.chislit * znamenat << "/" << d.znamenat * znamenat;
-	}
-	void Fmin(const Drob& d)
-	{
-		cout << "Min_Full:\t\b\b\b\b" << chislit * d.znamenat - d.chislit * znamenat << "/" << d.znamenat * znamenat;
-	}
-	void Fmult(const Drob& d)
-	{
-		cout << "Mult_Full:\t\b\b\b\b" << chislit * d.chislit << "/" << d.znamenat * znamenat;
-	}
-	void Fdel(const Drob& d)
-	{
-		cout << "Del_Full:\t\b\b\b\b" << chislit * d.znamenat << "/" << znamenat * d.chislit;
-	}
-
-	*/
-
-	/*Drob& operator +(const Drob& d2)
-	{
-		chislit = (chislit * d2.znamenat + d2.chislit * znamenat);
-		znamenat = d2.znamenat * znamenat;
-		return *this;
-	}
-
-	Drob& operator -(const Drob& d2)
-	{
-		chislit = (chislit * d2.znamenat - d2.chislit * znamenat);
-		znamenat = d2.znamenat * znamenat;
-		return *this;
-	}
-
-	Drob& operator *(const Drob& d2)
-	{
-		chislit = (chislit * d2.chislit);
-		znamenat = d2.znamenat * znamenat;
-		return *this;
-	}
-
-	Drob& operator /(const Drob& d2)
-	{
-		chislit = (chislit * d2.znamenat);
-		znamenat = d2.chislit * znamenat;
-		return *this;
-	}
-
-	Drob& operator ++()
-	{
-		znamenat++;
-		return *this;
-	}
-	Drob& operator ++(int)
-	{
-		Drob temp{ *this };
-		znamenat++;
-		return temp;
-	}
-
-	Drob& operator --()
-	{
-		znamenat--;
-		return *this;
-	}
-	Drob& operator --(int)
-	{
-		Drob temp{ *this };
-		znamenat--;
-		return temp;
-	}
-
-	friend bool operator ==(const Drob& d1, const Drob& d2);
-	friend ostream& operator <<(ostream& out, const Drob& d1);
-	friend istream& operator >>(istream& in, Drob& d1);
-};
-
-bool operator ==(const Drob& d1, const Drob& d2)
-{
-	return (d1.chislit / d1.znamenat) == (d2.chislit / d2.znamenat);
 }
 
-bool operator !=(const Drob& d1, const Drob& d2)
-{
-	return !(d1 == d2);
-}
-
-ostream & operator <<(ostream& out, const Drob& d1)
-{
-    out << d1.chislit << "/" << d1.znamenat << "\n";
-	return out;
-}
-istream & operator >>(istream& in, Drob& d1)
-{
-    in >> d1.chislit >> d1.znamenat;
-	return in;
-}
-	*/
-	}
-}
